0ce71cd8da9c92a44de396bf3c3d5f5c487b39d2
Bob <bob.yuan@zydab.net>
2025-07-14 16:51:35 +0800

    .

--- src/dispatchers/notify/docUpdate.handler.js
+++ src/dispatchers/notify/docUpdate.handler.js
@@ -3,11 +3,5 @@ import { sendTextMessage } from "../../services/lark.service.js";
 export default async function handleDocupdate(body) {
     console.log('接收到文档更新的script');
     console.log(body);
-
-    delete body.command;
-
-    console.log('更新后的参数');
-    console.log(body);
-
     return await sendTextMessage(body);
 }
