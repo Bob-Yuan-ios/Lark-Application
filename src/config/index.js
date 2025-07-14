6930033a01fcb839a1eb4b1dc12c26405721361d
Bob <bob.yuan@zydab.net>
2025-07-14 10:12:54 +0800

    .

--- src/config/index.js
+++ src/config/index.js
@@ -5,6 +5,6 @@
 export const PORT = process.env.PORT || 3000;
 export const TENANT_TOKEN = { value: '' };
 
-export const APP_ID = 'cli_a8ee51f3c5381010';
-export const APP_SECRET = 'rxPI5UYCLVb5gC3VdsDRxdjZl3C0UEZU';
-export const verificationToken = 'tkDYcJHl0TVaYd0hP3noOc3M1cdTShtD';
+export const APP_ID = '';
+export const APP_SECRET = '';
+export const verificationToken = '';
